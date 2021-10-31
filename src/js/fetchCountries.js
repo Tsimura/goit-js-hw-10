22a68d287bd2c55648c21a8c1171b732247b3035
Tsimura <chappa2828@gmail.com>
2021-10-31 09:46:20 +0200

    add fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,29 +1,49 @@
-export default class FetchCountriesApi {
-  constructor() {
-    this.searchValue = '';
-  }
+// export default class FetchCountriesApi {
+//   constructor() {
+//     this.searchValue = '';
+//   }
 
-  fetchCountries() {
-    const url = `https://restcountries.com/v3.1/name/${this.searchValue}?fullText=true`;
-    fetch(url)
-      .then(response => {
-        if (response.status === 404) {
-          alert('Перевір запит!!!!');
-        }
-        return response.json();
-      })
-      .then(country => console.log(country))
-      .catch(error => console.log(error));
-  }
+//   fetchCountries() {
+//     const url = `https://restcountries.com/v3.1/name/${this.searchValue}?fullText=true`;
+//     fetch(url)
+//       .then(response => {
+//         if (response.status === 404) {
+//           alert('Перевір запит!!!!');
+//         }
+//         return response.json();
+//       })
+//       .then(country => console.log(country))
+//       .catch(error => console.log(error));
+//   }
 
-  get searchParameter() {
-    return this.searchValue;
-  }
-  set searchParameter(newSearchValue) {
-    this.searchValue = newSearchValue;
-  }
+//   get searchParameter() {
+//     return this.searchValue;
+//   }
+//   set searchParameter(newSearchValue) {
+//     this.searchValue = newSearchValue;
+//   }
+// }
+
+// function fetchCountriesApi(searchValue) {
+//   const url = `https://restcountries.com/v3.1/name/${searchValue}`;
+
+//   return fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//     })
+//     .then(country => console.log(country));
+// }
+function fetchCountries(name) {
+  //Запрос на бек
+  const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+  return fetch(url).then(response => {
+    if (response.status === 404) {
+      alert('Oops, there is no country with that name!');
+    }
+    return response.json();
+  });
 }
 
-// fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags.svg,languages')
-
-// https://youtu.be/Fh8d14cY9AM?t=3124
+export default fetchCountries;
