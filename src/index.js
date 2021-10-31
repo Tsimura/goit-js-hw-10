91da15cbb0e21da57a6936ac9c6788547d274af2
Tsimura <chappa2828@gmail.com>
2021-10-31 13:03:35 +0200

    base

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 import getRefs from './js/get-refs';
+import fetchCountriesList from '../src/templates/country-list.hbs';
+import countryTemplate from '../src/templates/country-info.hbs';
 import fetchCountries from '../src/js/fetchCountries';
-import countriesTemplate from '../src/templates/country-list.hbs';
 
 const debounce = require('lodash.debounce');
 
@@ -14,87 +16,60 @@ refs.inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch(e) {
   const form = e.currentTarget;
   let searchQuery = document.getElementById('search-box').value.trim();
-  console.log(searchQuery);
-  fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
+  // console.log(searchQuery);
+  if (!searchQuery) {
+    onFetchError();
+    return;
+  }
+  fetchCountries(searchQuery).then(createCountryList).catch(onFetchError);
 }
 
 function renderCountryCard(country) {
-  //Рендер 1-ї карточки
-  const markup = countriesTemplate(country);
-  console.log(markup);
+  const markup = countryTemplate(country);
+  // console.log(markup);
   refs.countryInfo.innerHTML = markup;
   refs.inputEl.value = '';
 }
 
 function onFetchError(error) {
-  alert('Oops, there is no country with that name!');
+  Notiflix.Notify.failure('Oops, there is no country with that name!!!');
+  console.log('Oops, there is no country with that name!!!');
 }
 
 function createCountryList(numberOfCountries) {
-  console.log(numberOfCountries);
+  // console.log(numberOfCountries);
+  if (numberOfCountries.length <= 1) {
+    clear();
+    // console.log(country.name.official);
+    console.log(numberOfCountries[0].languages.name);
+    renderCountryCard(numberOfCountries);
+  }
+  if (numberOfCountries.length > 1 && numberOfCountries.length <= 10) {
+    clear();
+    numberOfCountries.forEach(country => {
+      const markup = fetchCountriesList(country.name.official);
+      console.log(country.name.official);
+      refs.countryList.insertAdjacentHTML(
+        'beforeend',
+        `<li class="country-list__info">
+        <img src='${country.flags.svg}' width='36' height='24' alt='${country.name.official}' class='country__img' />
+        ${country.name.official}</li>`,
+      );
+    });
+  }
+  if (numberOfCountries.length > 10) {
+    clear();
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    console.log('Too many matches found. Please enter a more specific name.');
+  }
 }
-
-//==============================Мусорка!!!
-
-// відправити запрос і перевірити кількість співпадінь
-// вивести список країн
-// якщо співпадння 1 вивести повну інформацію
-// якщо більше одного, просто вивести списком
-// якщо помилка, або забагато співпадінь - відповідне повідомлення
-// очищави відповідні поля
+function clear() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
 
 // name.official - полное имя страны
 // capital - столица
 // population - население
 // flags.svg - ссылка на изображение флага
 // languages - массив языков
-
-// function searchCountry(e) {
-//   let inputValue = e.target.value.trim();
-//   if (!inputValue) {
-//     alert('Перевір запит!!!');
-//     return;
-//   }
-//   getCountries(inputValue);
-// }
-
-// function createFullCountryInfo(country) {
-//   const markup = countriesTemplate(country);
-//   //добавити розмітку з повною інформацією про країну
-// }
-
-// function clearList() {}
-
-// function countriesFound() {}
-
-// function getCountries(value) {
-//   fetchCountriesApi(value).then(countries =>
-//     countriesFound(countries).catch(error => requestFailed(error)),
-//   );
-// }
-
-// function requestFailed() {
-//   //нужно очистить поля
-//   console.log('Oops, there is no country with that name!');
-// }
-
-//=====================================================================================
-// function searchCountry(event) {
-//   fetchCountriesApi.searchParameter = document.getElementById('search-box').value;
-//   fetchCountriesApi.fetchCountries();
-// }
-
-// function renderListCountry(country) {
-//   refs.countryInfo.innerHTML = '';
-//   if (country.length === 1) {
-//     console.log(country.length === 1);
-//     const newCountry = country[0];
-//     refs.countryInfo.insertAdjacentHTML('beforeend', countryTemplate(newCountry));
-//   }
-//   if (country.length <= 10 && country.length !== 1) {
-//     refs.countryList.insertAdjacentHTML(
-//       'beforeend',
-//       countryTemplate(country.map(country => country.name)),
-//     );
-//   }
-// }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,46 +1,9 @@
-// export default class FetchCountriesApi {
-//   constructor() {
-//     this.searchValue = '';
-//   }
-
-//   fetchCountries() {
-//     const url = `https://restcountries.com/v3.1/name/${this.searchValue}?fullText=true`;
-//     fetch(url)
-//       .then(response => {
-//         if (response.status === 404) {
-//           alert('Перевір запит!!!!');
-//         }
-//         return response.json();
-//       })
-//       .then(country => console.log(country))
-//       .catch(error => console.log(error));
-//   }
-
-//   get searchParameter() {
-//     return this.searchValue;
-//   }
-//   set searchParameter(newSearchValue) {
-//     this.searchValue = newSearchValue;
-//   }
-// }
-
-// function fetchCountriesApi(searchValue) {
-//   const url = `https://restcountries.com/v3.1/name/${searchValue}`;
-
-//   return fetch(url)
-//     .then(response => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//     })
-//     .then(country => console.log(country));
-// }
+import Notiflix from 'notiflix';
 function fetchCountries(name) {
-  //Запрос на бек
-  const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+  const url = `https://restcountries.com/v3.1/name/${name}`;
   return fetch(url).then(response => {
     if (response.status === 404) {
-      alert('Oops, there is no country with that name!');
+      Notiflix.Notify.failure('Oops, there is no country with that name!');
     }
     return response.json();
   });
